8698d694b6f1997150788d79c5a10e51406309ba
William Ma <whoiswillma@gmail.com>
2021-09-14 16:49:18 -0400

    luke: Add logging, tqdm for QOL

--- util.py
+++ util.py
@@ -1,10 +1,18 @@
 from datetime import datetime
 import torch
 
-def save_checkpoint(model, opt, epoch):
+
+def get_datetime_str():
+    return datetime.now().strftime('%m-%d@%H-%M')
+
+
+def save_checkpoint(model, opt, epoch, datetime_str=None):
+    if not datetime_str:
+        datetime_str = get_datetime_str()
+
     name = (type(model).__name__ 
         + '-Checkpoint-e{}-'.format(epoch) 
-        + datetime.now().strftime('%m-%d@%H-%M'))
+        + datetime_str)
     
     torch.save(
         {
@@ -14,8 +22,11 @@ def save_checkpoint(model, opt, epoch):
         }, 
         '{}.pt'.format(name)
     )
+
+    return name
     
 def load_checkpoint(model, opt, name):
     checkpoint = torch.load(name)
     model.load_state_dict(checkpoint['model_state_dict'])
     opt.load_state_dict(checkpoint['opt_state_dict'])
+
