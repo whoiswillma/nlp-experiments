f3d24f7df5c5494150024ffd479d32d398f673b0
William Ma <whoiswillma@gmail.com>
2021-10-13 18:48:07 -0400

    Add function for extracting named entity spans from bio tags

--- ner.py
+++ ner.py
@@ -1,4 +1,4 @@
-from typing import TypeVar
+from typing import TypeVar, Optional
 
 T = TypeVar('T')
 
@@ -56,3 +56,49 @@ def extract_nets_from_bio(tokens: list[str], tags: list[str]) -> list[tuple[str,
             raise Exception(f'Unexpected tag string {tag_str}')
         
     return named_entities_and_category
+
+
+def extract_named_entity_spans_from_bio(tags: list[str]) -> dict[str, list[tuple[int,int]]]:
+    """Convert BIO tags to named entity spans (inclusive) by type
+    """
+
+    named_entity_spans: dict[str, list[tuple[int, int]]] = {}
+
+    current_named_entity_start: Optional[int] = None
+    current_tag: Optional[str] = None
+
+    def add_current_named_entity_to_span(end_index: int):
+        assert current_tag is not None
+        assert current_named_entity_start is not None
+
+        if current_tag not in named_entity_spans:
+            named_entity_spans[current_tag] = []
+
+        named_entity_spans[current_tag].append((current_named_entity_start, end_index - 1))
+
+    for i, tag_str in enumerate(tags + ['O']):
+        bio = tag_str[:1]
+        tag = tag_str[2:]
+
+        if bio == 'B':
+            if current_tag is not None:
+                add_current_named_entity_to_span(i)
+
+            current_named_entity_start = i
+            current_tag = tag
+
+        elif bio == 'O':
+            if current_tag is not None:
+                add_current_named_entity_to_span(i)
+
+            current_named_entity_start = None
+            current_tag = None
+
+        elif bio == 'I':
+            if not (tag == current_tag):
+                raise ValueError(f'Invalid transition {current_tag} -> I-{tag}')
+
+        else:
+            raise ValueError(f'Unexpected tag string {tag_str}')
+
+    return named_entity_spans
