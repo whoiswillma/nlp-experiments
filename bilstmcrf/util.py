cc3236886e31f191093e75d9390c2da6d08ba327
ssc255 <ssc255@cornell.edu>
2021-10-23 20:36:32 -0400

    minor fix in util

--- bilstmcrf/util.py
+++ bilstmcrf/util.py
@@ -45,4 +45,61 @@ def build_mappings(examples: List[str]) -> Tuple[Dict[str, int], Dict[int, str]]
     for i, token in enumerate(sorted(vocab)):
         tokens_to_idx[token] = i + 2
         idx_to_tokens[i + 2] = token
-    return tokens_to_idx, idx_to_tokens
+    return tokens_to_idx,
+
+
+
+
+# dict from label string to token-level spans inclusive
+NamedEntityLabelSpans = dict[str, list[tuple[int, int]]]
+
+# dict from label id to token-level spans inclusive
+NamedEntityIdSpans = dict[int, list[tuple[int, int]]]
+
+NamedEntitySpans = Union[NamedEntityLabelSpans, NamedEntityIdSpans]
+
+
+def extract_named_entity_spans_from_bio(tags: list[str]) -> NamedEntityLabelSpans:
+    """Convert BIO tags to named entity spans (inclusive) by type
+    """
+
+    named_entity_spans: NamedEntityLabelSpans = {}
+
+    current_named_entity_start: Optional[int] = None
+    current_tag: Optional[str] = None
+
+    def add_current_named_entity_to_span(end_index: int):
+        assert current_tag is not None
+        assert current_named_entity_start is not None
+
+        if current_tag not in named_entity_spans:
+            named_entity_spans[current_tag] = []
+
+        named_entity_spans[current_tag].append((current_named_entity_start, end_index - 1))
+
+    for i, tag_str in enumerate(tags + ['O']):
+        bio = tag_str[:1]
+        tag = tag_str[2:]
+
+        if bio == 'B':
+            if current_tag is not None:
+                add_current_named_entity_to_span(i)
+
+            current_named_entity_start = i
+            current_tag = tag
+
+        elif bio == 'O':
+            if current_tag is not None:
+                add_current_named_entity_to_span(i)
+
+            current_named_entity_start = None
+            current_tag = None
+
+        elif bio == 'I':
+            if not (tag == current_tag):
+                raise ValueError(f'Invalid transition {current_tag} -> I-{tag}')
+
+        else:
+            raise ValueError(f'Unexpected tag string {tag_str}')
+
+    return named_entity_spans
