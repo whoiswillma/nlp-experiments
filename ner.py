aedc2c02bea837def1b4646bfc0e957a689f8f09
John Chung <jc2739@g2-login-01.coecis.cornell.edu>
2021-11-10 18:29:07 -0500

    feat: add github action yml file for lint check, update Readme & apply lint

--- ner.py
+++ ner.py
@@ -1,19 +1,23 @@
 import logging
 from typing import TypeVar, Optional, Union
 
-T = TypeVar('T')
+T = TypeVar("T")
 
 
-def extract_nets_from_chunks(tokens: list[str], labels: list[T], null_label: T) -> list[tuple[str, T]]:
+def extract_nets_from_chunks(
+    tokens: list[str], labels: list[T], null_label: T
+) -> list[tuple[str, T]]:
     named_entity_and_type: list[tuple[str, T]] = []
 
     current_named_entity = []
     current_label = null_label
 
-    for token, label in list(zip(tokens, labels)) + [('', null_label)]:
+    for token, label in list(zip(tokens, labels)) + [("", null_label)]:
         if current_label != label:
             if current_label != null_label:
-                named_entity_and_type.append((' '.join(current_named_entity), current_label))
+                named_entity_and_type.append(
+                    (" ".join(current_named_entity), current_label)
+                )
             current_named_entity = []
 
         current_label = label
@@ -27,36 +31,40 @@ def extract_nets_from_bio(tokens: list[str], tags: list[str]) -> list[tuple[str,
     """
 
     named_entities_and_category = []
-    
+
     current_named_entity = []
     current_tag = None
-    
+
     for token, tag_str in zip(tokens, tags):
         bio = tag_str[:1]
         tag = tag_str[2:]
-        
-        if bio == 'B':
+
+        if bio == "B":
             if current_tag:
-                named_entities_and_category.append((' '.join(current_named_entity), current_tag))
-            
+                named_entities_and_category.append(
+                    (" ".join(current_named_entity), current_tag)
+                )
+
             current_named_entity = [token]
             current_tag = tag
-            
-        elif bio == 'O':
+
+        elif bio == "O":
             if current_tag:
-                named_entities_and_category.append((' '.join(current_named_entity), current_tag))
-            
+                named_entities_and_category.append(
+                    (" ".join(current_named_entity), current_tag)
+                )
+
             current_named_entity = []
             current_tag = None
-            
-        elif bio == 'I':
+
+        elif bio == "I":
             if not (tag == current_tag):
-                raise Exception(f'Invalid transition {current_tag} -> I-{tag}')
+                raise Exception(f"Invalid transition {current_tag} -> I-{tag}")
             current_named_entity.append(token)
-            
+
         else:
-            raise Exception(f'Unexpected tag string {tag_str}')
-        
+            raise Exception(f"Unexpected tag string {tag_str}")
+
     return named_entities_and_category
 
 
@@ -86,39 +94,40 @@ def extract_named_entity_spans_from_bio(tags: list[str]) -> NamedEntityLabelSpan
         if current_tag not in named_entity_spans:
             named_entity_spans[current_tag] = []
 
-        named_entity_spans[current_tag].append((current_named_entity_start, end_index - 1))
+        named_entity_spans[current_tag].append(
+            (current_named_entity_start, end_index - 1)
+        )
 
-    for i, tag_str in enumerate(tags + ['O']):
+    for i, tag_str in enumerate(tags + ["O"]):
         bio = tag_str[:1]
         tag = tag_str[2:]
 
-        if bio == 'B':
+        if bio == "B":
             if current_tag is not None:
                 add_current_named_entity_to_span(i)
 
             current_named_entity_start = i
             current_tag = tag
 
-        elif bio == 'O':
+        elif bio == "O":
             if current_tag is not None:
                 add_current_named_entity_to_span(i)
 
             current_named_entity_start = None
             current_tag = None
 
-        elif bio == 'I':
+        elif bio == "I":
             if not (tag == current_tag):
-                raise ValueError(f'Invalid transition {current_tag} -> I-{tag}')
+                raise ValueError(f"Invalid transition {current_tag} -> I-{tag}")
 
         else:
-            raise ValueError(f'Unexpected tag string {tag_str}')
+            raise ValueError(f"Unexpected tag string {tag_str}")
 
     return named_entity_spans
 
 
 def extract_named_entity_spans_from_chunks(
-        tags: list[str],
-        nonentity_label: str='O'
+    tags: list[str], nonentity_label: str = "O"
 ) -> NamedEntityLabelSpans:
     """Extract named entity label spans from chunked predictions
 
@@ -146,13 +155,7 @@ def extract_named_entity_spans_from_chunks(
 
 
 class NERBinaryConfusionMatrix:
-
-    def __init__(
-            self,
-            tp: int = 0,
-            fn: int = 0,
-            fp: int = 0
-    ):
+    def __init__(self, tp: int = 0, fn: int = 0, fp: int = 0):
         # true positives (pred and gold both contain span of same type)
         self.tp = tp
 
@@ -169,20 +172,24 @@ class NERBinaryConfusionMatrix:
         return self.tp == other.tp and self.fn == other.fn and self.fp == other.fp
 
     def __repr__(self):
-        return f'{{ tp: {self.tp}, fn: {self.fn}, fp: {self.fp} }}'
+        return f"{{ tp: {self.tp}, fn: {self.fn}, fp: {self.fp} }}"
 
     def precision(self):
         if self.tp + self.fp == 0:
-            logging.warning(f'Cannot compute precision when tp ({self.tp}) + fp ({self.fn}) == 0.')
-            logging.warning('Returning 0 for now')
+            logging.warning(
+                f"Cannot compute precision when tp ({self.tp}) + fp ({self.fn}) == 0."
+            )
+            logging.warning("Returning 0 for now")
             return 0
 
         return self.tp / (self.tp + self.fp)
 
     def recall(self):
         if self.tp + self.fn == 0:
-            logging.warning(f'Cannot compute recall when tp ({self.tp}) + fn ({self.fn}) == 0.')
-            logging.warning('Returning 0 for now')
+            logging.warning(
+                f"Cannot compute recall when tp ({self.tp}) + fn ({self.fn}) == 0."
+            )
+            logging.warning("Returning 0 for now")
             return 0
 
         return self.tp / (self.tp + self.fn)
@@ -191,27 +198,29 @@ class NERBinaryConfusionMatrix:
         r = self.recall()
         p = self.precision()
         if r + p == 0:
-            logging.warning(f'Cannot compute f1 when recall ({r}) + precision ({p}) == 0.')
-            logging.warning('Returning 0 for now')
+            logging.warning(
+                f"Cannot compute f1 when recall ({r}) + precision ({p}) == 0."
+            )
+            logging.warning("Returning 0 for now")
             return 0
 
         return 2 * p * r / (p + r)
 
     def metrics(self):
         return {
-            'tp': self.tp,
-            'fn': self.fn,
-            'fp': self.fp,
-            'precision': self.precision(),
-            'recall': self.recall(),
-            'f1': self.f1()
+            "tp": self.tp,
+            "fn": self.fn,
+            "fp": self.fp,
+            "precision": self.precision(),
+            "recall": self.recall(),
+            "f1": self.f1(),
         }
 
 
 def compute_binary_confusion_matrix_from_bio(
-        pred_bio: list[str],
-        gold_bio: list[str],
-        accumulate: Optional[NERBinaryConfusionMatrix] = None
+    pred_bio: list[str],
+    gold_bio: list[str],
+    accumulate: Optional[NERBinaryConfusionMatrix] = None,
 ) -> NERBinaryConfusionMatrix:
     assert len(pred_bio) == len(gold_bio)
 
@@ -219,16 +228,14 @@ def compute_binary_confusion_matrix_from_bio(
     gold_ner_spans = extract_named_entity_spans_from_bio(gold_bio)
 
     return compute_binary_confusion_from_named_entity_spans(
-        pred_ner_spans,
-        gold_ner_spans,
-        accumulate=accumulate
+        pred_ner_spans, gold_ner_spans, accumulate=accumulate
     )
 
 
 def compute_binary_confusion_matrix_from_batched_bio(
-        pred_bio_batched: list[list[str]],
-        gold_bio_batched: list[list[str]],
-        accumulate: Optional[NERBinaryConfusionMatrix] = None
+    pred_bio_batched: list[list[str]],
+    gold_bio_batched: list[list[str]],
+    accumulate: Optional[NERBinaryConfusionMatrix] = None,
 ) -> NERBinaryConfusionMatrix:
     assert len(pred_bio_batched) == len(gold_bio_batched)
 
@@ -242,9 +249,9 @@ def compute_binary_confusion_matrix_from_batched_bio(
 
 
 def compute_binary_confusion_from_named_entity_spans(
-        pred_ne_spans: NamedEntitySpans,
-        gold_ne_spans: NamedEntitySpans,
-        accumulate: Optional[NERBinaryConfusionMatrix] = None
+    pred_ne_spans: NamedEntitySpans,
+    gold_ne_spans: NamedEntitySpans,
+    accumulate: Optional[NERBinaryConfusionMatrix] = None,
 ) -> NERBinaryConfusionMatrix:
 
     if accumulate is None:
